34c28646ae6d1a594e9b6a3e3ae9b093e9a1216f
Siddharth Mehta <50422188+Siddharthm10@users.noreply.github.com>
2020-07-02 14:56:29 +0530

    Update Reverse Integer.cpp

--- Easy/Reverse Integer.cpp
+++ Easy/Reverse Integer.cpp
@@ -1,2 +1,21 @@
 // Question Statement:
 // Given a 32-bit signed integer, reverse digits of an integer.
+
+class Solution {
+public:
+    int reverse(int x) {
+        
+        if((x>=(-pow(2,31))) && (x<=pow(2,31)-1)){
+            vector<int> digits {};
+            int length = log10(x)+1;
+            for(size_t i=0; i<length;i++){
+//                 example = 123
+//                           210
+                digits.push_back(x%pow(10,i+1))
+            }
+        
+        }
+        else
+            return 0;
+    }
+};
