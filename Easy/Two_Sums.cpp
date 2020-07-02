c64148c42a2e2597467b5087a918e2f7c9fb9af0
Siddharth Mehta <50422188+Siddharthm10@users.noreply.github.com>
2020-07-02 12:38:30 +0530

    Update Two_Sums.cpp

--- Easy/Two_Sums.cpp
+++ Easy/Two_Sums.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int x=0;
+        std::unordered_map<int,int> a;
+        for(int i=0;i<nums.size();i++){
+            x=target-nums[i];
+            if(a.find(x)!=a.end()){
+                return {a.find(x)->second,i};
+            }else{
+                a[nums[i]]=i;
+            }
+        }
+    return{};
+    }
+};
+// It was hard as I was thinking of using two for loops but later on I found this solution with O(n) time complexity.
