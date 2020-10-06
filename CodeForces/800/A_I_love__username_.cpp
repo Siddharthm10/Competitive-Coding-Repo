015b216e163f6d69bf317c512658879a0d03991b
siddharthm10 <mehtasid130499@gmail.com>
2020-10-07 00:46:20 +0530

    October 6

--- CodeForces/800/A_I_love__username_.cpp
+++ CodeForces/800/A_I_love__username_.cpp
@@ -6,24 +6,29 @@
 #define fo(i,n,j) for(int i=0; i<n; i=i+j)
 using namespace std;
 
+int maximum_calculation(int* points, int index){
+    int max = *max_element(points,points+index);
+    return max;
+}
+int minimum_calculation(int* points, int index){
+    int min = *min_element(points, points+index);
+    return min;
+}
+
 int main()
 {
     int n;
     cin>>n;
     int points[n];
     fo(i,n,1) cin>>points[i];
-    //write code here
+    // finding max value in points before index 
     int amazing=0;
     fo(i,n,1){
-        if(i>0){
-            fo(j,i,1){
-                if(points[i]>points[j])
-                    if(j==i-1){amazing++;cout<<1<<" ";}
-                if(points[i]<points[j]) 
-                    if(j==i-1){amazing++;cout<<2<<" ";}
-            }
-        }
+        if(points[i]>maximum_calculation(points,i)) amazing++;
+        if(points[i]<minimum_calculation(points,i)) amazing++;
     }
+    
+    
     cout<<(amazing);
     return 0;
 }
