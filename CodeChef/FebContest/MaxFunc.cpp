5fa90e97a5f8ec1025aba6f23e5348b93953b2d5
Siddharthm10 <siddharth.mehtaid@gmail.com>
2021-02-07 11:58:50 +0530

    Feb 7

--- CodeChef/FebContest/MaxFunc.cpp
+++ CodeChef/FebContest/MaxFunc.cpp
@@ -4,29 +4,30 @@
 #include <stdio.h>
 #include <iostream>
 #define pi 3.1415926535
+#define ll long long int
 #define fu(i,n) for(int i=0; i<n; i++)
 #define fd(i,n) for(int i=n; i>=0; i--)
 
 using namespace std;
 
-int maxFunc(vector<int> A){
-    int maxValue=0;
-        
+void maxFunc(){
+    ll n;
+    cin>>n;
+    ll a[n];
+    for(ll j=0; j<n; j++){
+        cin>>a[j];
+    }
+    sort(a, a+n);
+    ll maxValue = 2*(a[n-1] - a[0]);
+    cout<<maxValue<<endl;
 }
 
 int main()
 {
-    int T;
+    ll T;
     cin>>T;
-    int N;
-    int temp;
-    for(int i=0; i<T; i++){
-        cin>>N;
-        vector<int> A(N);
-        for(int j=0; j<N; j++)
-            cin>>A[j];
-        cout<<maxFunc(A)<<endl;
-    }
-    //write code here
+    for(ll i=0; i<T; i++)
+        maxFunc();
+    
     return 0;
 }
