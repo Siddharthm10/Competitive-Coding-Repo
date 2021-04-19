8fc9b222a660355325d478036cb031821d256139
Siddharthm10 <siddharth.mehtaid@gmail.com>
2021-04-19 23:54:01 +0530

    April 19

---
+++ LeetCode/HashTables/Easy/singleNumber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> m;
+        if(nums.size()==1) return nums[0];
+        for(int i=0; i<nums.size(); i++){
+            m[nums[i]]++;
+        }
+        for(auto x:m){
+            if(x.second==1) return x.first;
+        }
+        return 0;
+        
+    }
+};
