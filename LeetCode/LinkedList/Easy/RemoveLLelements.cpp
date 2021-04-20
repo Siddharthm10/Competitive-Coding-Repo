4cc59821b129dd74bc0b0af0ced49bae65e84f4f
Siddharthm10 <siddharth.mehtaid@gmail.com>
2021-04-21 00:41:41 +0530

    April 21

--- LeetCode/LinkedList/Easy/RemoveLLelements.cpp
+++ LeetCode/LinkedList/Easy/RemoveLLelements.cpp
@@ -32,3 +32,36 @@ public:
         return head;
     }
 };
+
+
+
+//Better Solution is 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head==NULL) return NULL;
+        while(head->val==val){
+            head = head->next;
+            if(head==NULL) return NULL;
+        }
+        ListNode* temp = head;
+
+        while(temp->next!=NULL){
+            if(temp->next->val == val){
+                temp->next = temp->next->next;
+            }else
+                temp = temp->next;
+        }
+        return head;
+    }
+};
