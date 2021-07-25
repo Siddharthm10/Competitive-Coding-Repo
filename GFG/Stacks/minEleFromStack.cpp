d4e59d9af4ee0eb310e368fce8bb47c69406a54f
Siddharth Mehta <50422188+Siddharthm10@users.noreply.github.com>
2021-07-25 23:06:23 +0530

    July 25

--- GFG/Stacks/minEleFromStack.cpp
+++ GFG/Stacks/minEleFromStack.cpp
@@ -1,41 +1,21 @@
-/*
-The structure of the class is as follows
-class _stack{
-stack<int> s;
-int minEle;
-public :
-    int getMin();
-    int pop();
-    void push(int);
-};
-*/
-
 /*returns min element from stack*/
-int _stack :: getMin()
-{
-   //Your code here
-   if(s.empty()) return -1;
-   return s.top();
+ int _stack :: getMin(){
+    if(s.empty()) return -1;
+    return s.top();
 }
-
 /*returns poped element from stack*/
-int _stack ::pop()
-{
-   //Your code here
+int _stack ::pop(){
    if(s.empty()) return -1;
    s.pop();
-   int top = s.top();
+   int op = s.top();
    s.pop();
-   if(!s.empty()) minEle = top;
-   return top;
+   if(!s.empty()) minEle = s.top();
+   return op;
 }
-
 /*push element x into the stack*/
-void _stack::push(int x)
-{
-   //Your code here
-   if(s.empty()) minEle = x;
-   if(x<minEle) minEle = x;
+void _stack::push(int x){
+   if(s.empty()) minEle=x;
+   if(x < minEle) minEle = x;
    s.push(x);
    s.push(minEle);
 }
