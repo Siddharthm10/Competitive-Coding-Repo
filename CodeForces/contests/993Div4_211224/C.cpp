ca9d5fc6b602afed2122627600fc1e71b964cf33
siddharthm10 <siddharth.mehtaid@gmail.com>
2024-12-21 02:54:38 -0500

    Added contest 993

---
+++ CodeForces/contests/993Div4_211224/C.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        //write code here
+        int m;
+        cin>>m;
+    
+        int a, b, c;
+        cin>>a>>b>>c;
+
+        //a - row 1
+        int row1 = m;
+        int row2 = m;
+        int ans = 0;
+
+        cout<<(min(a, row1) + min(b,row2) + min(c, row1+row2-min(a,row1)-min(b,row2)))<<'\n';
+    }
+    return 0;
+}
